cd1ad0d3653168a7bd1b880ff530f1b9622d31f6
Mike Rosset <mike.rosset@gmail.com>
2021-09-05 13:00:30 -0700

    (prospect ffi) new procedure get-data-basic.
    Limited support for libblkmaker_get_data

--- scheme/prospect/ffi.scm
+++ scheme/prospect/ffi.scm
@@ -20,6 +20,7 @@
   #:use-module (prospect util)
   #:use-module (prospect rpc)
   #:use-module (oop goops)
+  #:use-module (json)
   #:use-module (unit-test)
   #:use-module (system foreign)
   #:use-module (system foreign-library))
@@ -37,6 +38,21 @@
    #f
    "gnu_get_libc_version"
    #:return-type '*))
+
+;; Jansson
+(define json-dumps
+  (foreign-library-function
+   libjansson
+   "json_dumps"
+   #:return-type '*
+   #:arg-types (list '* size_t)))
+
+(define json-loads
+  (foreign-library-function
+   libjansson
+   "json_loads"
+   #:return-type '*
+   #:arg-types (list '* size_t '*)))
 
 (define make-template
   (foreign-library-function
@@ -58,25 +74,64 @@
    #:return-type '*
    #:arg-types (list uint32 '*)))
 
+(define add-jansson
+  (foreign-library-function
+   libblkmaker-jansson
+   "blktmpl_add_jansson"
+   #:return-type '*
+   #:arg-types (list '* '* long)))
+
+(define get-data
+  (foreign-library-function
+   libblkmaker-jansson
+   "blkmk_get_data"
+   #:return-type size_t
+   #:arg-types (list '* '* size_t long '* '*)))
+
+(define get-data-basic
+  (foreign-library-function
+   libblkmaker-jansson
+   "blkmk_get_data_basic"
+   #:return-type '*
+   #:arg-types (list '* '* intptr_t)))
+
 (define (request->string req)
   "Returns the json string for @var{req}"
   (pointer->string (json-dumps req 2)))
 
-(define json-dumps
-  (foreign-library-function
-   libjansson
-   "json_dumps"
-   #:return-type '*
-   #:arg-types (list '* size_t)))
+(define-method (test-get-data (self <test-ffi>))
+  (let* ((tmpl (make-template))
+	 (req  (begin (assert-false (null-pointer? tmpl))
+		      (request-jansson (add-caps tmpl)
+				       %null-pointer)))
+	 (rstr (begin (assert-true (not (null-pointer? req)))
+		      (display (request->string req))
+		      (post-json (request->string req))))
+	 (res  (begin (assert-true (string? rstr))
+		      (call-with-output-file "data.json"
+			(lambda (port)
+			  (let ((j (scm->json-string (json-string->scm rstr) #:pretty #t)))
+			    (display j port))))
+		      (json-loads (string->pointer rstr)
+				  0
+			  %null-pointer)))
+	 (data (begin (assert-false (null-pointer? res))
+		      ;; (add-jansson tmpl res (current-time))
+		      (get-data-basic tmpl res 0)
+		      )))
+    #t
+    ;; (assert-true (string? (pointer->string data)))
+    ;; (assert-true (string? (dimi (pointer->string data))))
+    ))
 
 (define-method (test-template (self <test-ffi>))
-  (assert-true (pointer? (make-template)))    )
+  (assert-true (pointer? (make-template))))
 
-(define-method (test-request (self <test-ffi>))
-  (let* ((tmpl  (make-template))
-	 (req   (request-jansson (add-caps tmpl)
-				 %null-pointer))
-	 (res  (post-json (request->string req))))
-    (assert-true (pointer? req))
-    (assert-true (string? (request->string req)))
-    (assert-true (string? res))))
+;; (define-method (test-request (self <test-ffi>))
+;;   (let* ((tmpl  (make-template))
+;;	 (req   (request-jansson (add-caps tmpl)
+;;				 %null-pointer))
+;;	 (res  (post-json (dimi (request->string req)))))
+;;     (assert-true (pointer? req))
+;;     (assert-true (string? (request->string req)))
+;;     (assert-true (string?  res))))
